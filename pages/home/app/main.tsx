67daa8e6ce0a472b181bf442da43277452fe7143
mewhhaha <kopatheonlyone@hotmail.com>
2022-08-07 02:20:16 +0200

    Remove 404 and update redirect in routes

--- pages/home/app/main.tsx
+++ pages/home/app/main.tsx
@@ -1,7 +1,6 @@
 import React from "react";
 import ReactDOM from "react-dom/client";
 import App from "./routes";
-import "@unocss/reset/tailwind.css";
 import "uno.css";
 
 const el = document.getElementById("root");
--- pages/home/remix.config.js
+++ pages/home/remix.config.js
@@ -4,14 +4,5 @@
 module.exports = {
   serverBuildTarget: "cloudflare-pages",
   server: "./server.js",
-  // devServerBroadcastDelay: 1000,
   ignoredRouteFiles: ["**/.*"],
-  // appDirectory: "src",
-  // assetsBuildDirectory: "../public/build",
-  // serverBuildPath:
-  //   process.env.NODE_ENV === "production"
-  //     ? "../functions/[[path]].js"
-  //     : undefined,
-  // publicPath: "/build/",
-  // devServerPort: 8002
 };
